d62c9c9f960e7cff0866d4df23c654ce7a701d64
lblokhin <lenin133@yandex.ru>
2021-03-02 18:40:47 +0300

    Scikit Learn training magic

--- sage_maker_magic/sage_maker_kernel/kernelmagics.py
+++ sage_maker_magic/sage_maker_kernel/kernelmagics.py
@@ -19,6 +19,7 @@ import boto3
 ## Sage Maker Estimator
 from sagemaker.pytorch import PyTorch
 from sagemaker.tensorflow import TensorFlow
+from sagemaker.sklearn.estimator import SKLearn
 
 ## Sage Maker processing
 from sagemaker.spark.processing import PySparkProcessor
@@ -254,6 +255,56 @@ class PyTorchEstimatorMagics(CommonEstimatorMagics):
         print(json.dumps(self.method_matcher[self.args.pop('method')](), sort_keys=True, indent=4, default=str))
 
 
+@magics_class
+class SKLearnEstimatorMagics(CommonEstimatorMagics):
+    """
+        Scikit Learn magic class.
+    """
+    def __init__(self, shell, data=None):
+        super(SKLearnEstimatorMagics, self).__init__(shell)
+        self.RuntimeClass = SKLearn
+
+    def _get_config(self):
+        conf = super(SKLearnEstimatorMagics, self)._get_config()
+        return conf.get_config('sklearn')
+
+    @magic_arguments()
+    @argument_group(title='methods', description=None)
+    @argument('method', type=str, choices=['submit', 'list', 'status', 'logs', 'delete', 'show_defaults'])
+    @argument_group(title='submit', description=None)
+    @argument('--estimator_name', type=str, help='estimator shell variable name')
+    @argument('--entry_point', type=str, help='notebook local code file')
+    @argument('--source_dir', type=str, help='Path (absolute, relative or an S3 URI) to a directory with any other training source code dependencies aside from the entry point file (default: None). If source_dir is an S3 URI, it must point to a tar.gz file. Structure within this directory are preserved when training on Amazon SageMaker.')
+    @argument('--role', type=str, help='An AWS IAM role (either name or full ARN). The Amazon SageMaker training jobs and APIs that create Amazon SageMaker endpoints use this role to access training data and model artifacts. After the endpoint is created, the inference code might use the IAM role, if it needs to access an AWS resource.')
+    @argument('--framework_version', type=str, help='Scikit-learn version you want to use for executing your model training code. List of supported versions: https://github.com/aws/sagemaker-python-sdk#sklearn-sagemaker-estimators')
+    @argument('--py_version', type=str, help='Python version')
+    @argument('--instance_type', type=str, help='Type of EC2 instance to use for training, for example, ‘ml.c4.xlarge’.')
+    @argument('--instance_count', type=int, help='Number of Amazon EC2 instances to use for training.')
+    @argument('--output_path', type=str, help='S3 location for saving the training result (model artifacts and output files). If not specified, results are stored to a default bucket. If the bucket with the specific name does not exist, the estimator creates the bucket during the fit() method execution.')
+    @argument('--hyperparameters', type=hyperparameters, help='Hyperparameters are passed to your script as arguments and can be retrieved with an argparse.', metavar='FOO:1,BAR:0.555,BAZ:ABC | \'FOO : 1, BAR : 0.555, BAZ : ABC\'')
+    @argument('--channel_training', type=str, help='A string that represents the path to the directory that contains the input data for the training channel. ')
+    @argument('--channel_testing', type=str, help='A string that represents the path to the directory that contains the input data for the testing channel. ')
+    @argument_group(title='submit-spot', description=None)
+    @argument('--use_spot_instances', type=bool, help='Specifies whether to use SageMaker Managed Spot instances for training. If enabled then the max_wait arg should also be set. More information: https://docs.aws.amazon.com/sagemaker/latest/dg/model-managed-spot-training.html ', nargs='?', const=True)
+    @argument('--max_wait', type=int, help='Timeout in seconds waiting for spot training instances (default: None). After this amount of time Amazon SageMaker will stop waiting for Spot instances to become available (default: None).')
+    @argument_group(title='submit-metrics', description=None)
+    @argument('--enable_sagemaker_metrics', type=bool, help='Enables SageMaker Metrics Time Series. For more information see: https://docs.aws.amazon.com/sagemaker/latest/dg/API_AlgorithmSpecification.html# SageMaker-Type-AlgorithmSpecification-EnableSageMakerMetricsTimeSeries ', nargs='?', const=True)
+    @argument('--metric_definitions', type=metric_definitions, nargs='*', help='A list of dictionaries that defines the metric(s) used to evaluate the training jobs. Each dictionary contains two keys: ‘Name’ for the name of the metric, and ‘Regex’ for the regular expression used to extract the metric from the logs. This should be defined only for jobs that don’t use an Amazon algorithm.', metavar="\'Name: loss, Regex: Loss = (.*?);\'")
+    @argument_group(title='list', description=None)
+    @argument('--name_contains', type=str, help='', default='scikit-learn')
+    @argument('--max_result', type=str, help='', default=10)
+    @cell_magic
+    @line_magic
+    @needs_local_scope
+    def sklearn(self, line, cell="", local_ns=None):
+        """
+            Scikit Learn magic command.
+        """
+        self.cell = cell
+        self.args = vars(parse_argstring(self.sklearn, line))
+        print(json.dumps(self.method_matcher[self.args.pop('method')](), sort_keys=True, indent=4, default=str))
+
+
 class CommonProcessorMagics(CommonMagics):
     """
     Common Processor magic class.
@@ -347,4 +398,5 @@ class PySparkProcessorMagics(CommonProcessorMagics):
 def load_ipython_extension(ipython):
     ipython.register_magics(TensorFlowEstimatorMagics)
     ipython.register_magics(PyTorchEstimatorMagics)
+    ipython.register_magics(SKLearnEstimatorMagics)
     ipython.register_magics(PySparkProcessorMagics)
